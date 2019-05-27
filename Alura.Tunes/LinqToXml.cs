0c659d574149b3030e73acb545c0de79ad976817
luis gomes <luisgustavogomes@outlook.com>
2019-05-27 12:55:59 -0300

    TrabalhandoComLinqToXml

---
+++ Alura.Tunes/LinqToXml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Alura.Tunes
+{
+    public class LinqToXml
+    {
+        public XElement Root { get; set; }
+
+        public LinqToXml()
+        {
+            Root = XElement.Load(@"Data\AluraTunes.xml");
+        }
+
+        public void LacoLinqNativoComXmlSimples()
+        {
+
+            Console.WriteLine("LacoLinqNativoComXmlSimples");
+            var queryGeneros = from g in Root.Element("Generos").Elements("Genero") select g;
+            foreach (var item in queryGeneros)
+            {
+                Console.WriteLine("{0}\t{1}", item.Element("GeneroId").Value, item.Element("Nome").Value);
+            }
+            LinqToObjects.Espaco();
+        }
+
+        public void LacoLinqNativoComXmlJoin()
+        {
+            Console.WriteLine("LacoLinqNativoComXmlJoin");
+            var query = from g in Root.Element("Generos").Elements("Genero")
+                        join m in Root.Element("Musicas").Elements("Musica") on g.Element("GeneroId").Value equals m.Element("GeneroId").Value
+                        select new
+                        {
+                            Musica = m.Element("Nome").Value,
+                            Genero = g.Element("Nome").Value
+                        };
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+            LinqToObjects.Espaco();
+        }
+
+
+    }
+}
--- Alura.Tunes/Program.cs
+++ Alura.Tunes/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace Alura.Tunes
 {
@@ -8,128 +9,23 @@ namespace Alura.Tunes
     {
         static void Main(string[] args)
         {
-            var generos = new List<Genero>
-            {
-                new Genero (1, "Rock"),
-                new Genero (2, "Reggae"),
-                new Genero (3, "Rock Progressivo"),
-                new Genero (4, "Punk Rock"),
-                new Genero (5, "Clássica"),
-            };
-
-            var musicas = new List<Musica>
-            {
-                new Musica ( 1, "Sweet Child O'Mine" , 1),
-                new Musica ( 2, "I Shoot The Sheriff", 2),
-                new Musica ( 3, "Danúbio Azul"       , 5)
-            };
-
-
-            LacoComCondicional(generos);
-            LacoComLinqNativoSemWhere(generos);
-            LacoComLinqNativoComWhere(generos);
-            NewMethod(generos, musicas);
-
+            TrabalhandoComLinqToXml();
         }
 
-        private static void NewMethod(List<Genero> generos, List<Musica> musicas)
+        private static void TrabalhandoComLinqToXml()
         {
-            Console.WriteLine("LacoComLinqNativoComJoin");
-            var query = from m in musicas
-                        join g in generos on m.GeneroId equals g.Id
-                        select new { m, g };
-
-            foreach (var item in query)
-            {
-                Console.WriteLine("{0}\t{1}\t{2}", item.m.Id, item.m.Nome, item.g.Nome);
-            }
-            Espaco();
+            var linqToXml = new LinqToXml();
+            linqToXml.LacoLinqNativoComXmlSimples();
+            linqToXml.LacoLinqNativoComXmlJoin();
         }
 
-        private static void LacoComLinqNativoComWhere(List<Genero> generos)
+        public static void TrabalhandoComLinqToObjects()
         {
-            Console.WriteLine("laço com linq nativo com where");
-            var queryLinq = from g in generos where g.Nome.Contains("Rock") select g;
-            foreach (var item in queryLinq)
-            {
-                Impressao(item);
-            }
-            Espaco();
-        }
-
-        private static void LacoComLinqNativoSemWhere(List<Genero> generos)
-        {
-            Console.WriteLine("laço com linq nativo ");
-            var query = from g in generos select g;
-
-            foreach (var item in query)
-            {
-                Impressao(item);
-            }
-            Espaco();
-        }
-
-        private static void LacoComCondicional(List<Genero> generos)
-        {
-            Console.WriteLine("Laço com Condicional");
-            foreach (var item in generos)
-            {
-                if (item.Nome.Contains("Rock"))
-                {
-                    Impressao(item);
-                }
-            }
-            Espaco();
-        }
-
-        private static void Impressao(Object item)
-        {
-            Console.WriteLine(item);
-        }
-
-        private static void Espaco()
-        {
-            Console.WriteLine();
-        }
-    }
-
-    public class Genero
-    {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public Genero(int id, string nome)
-        {
-            Id = id;
-            Nome = nome;
-        }
-
-        public override string ToString()
-        {
-            return $"Id: {Id} \t Nome: {Nome}";
-        }
-
-
-    }
-
-    public class Musica
-    {
-
-        public int Id { get; set; }
-        public String Nome { get; set; }
-        public int GeneroId { get; set; }
-
-
-        public Musica(int id, string nome, int generoId)
-        {
-            Id = id;
-            Nome = nome;
-            GeneroId = generoId;
-        }
-
-
-        public override string ToString()
-        {
-            return $"Id: {Id} \t Nome: {Nome} \t IdGenero: {GeneroId}";
+            var linqToObjects = new LinqToObjects();
+            linqToObjects.LacoComCondicional();
+            linqToObjects.LacoComLinqNativoSemWhere();
+            linqToObjects.LacoComLinqNativoComWhere();
+            linqToObjects.LacoComLinqNativoComJoin();
         }
     }
 }
